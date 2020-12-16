15eafeee75ac9dc37c3e661e134c30c5967efe8b
prajwallplayz <72447668+prajwallplayz@users.noreply.github.com>
2020-12-16 14:06:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var database;
+var back_img;
+var gameState =0;
+var playerCount = 0;
+var allPlayers;
+var M = 500;
+var player, form,game;
+var player1,player2;
+var players;
+var fruits;
+var fruitGroup;
+var fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
+var player_img;
+
+
+function preload(){
+  back_img = loadImage("jungle.jpg");
+  player_img = loadImage("basket2.png");
+  fruit1_img = loadImage("apple2.png");
+  fruit2_img = loadImage("banana2.png");
+  fruit3_img = loadImage("melon2.png");
+  fruit4_img = loadImage("orange2.png");
+  fruit5_img = loadImage("pineapple2.png");
+  fruitGroup = new Group();
+}
+function setup() {
+  createCanvas(1000, 600);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+  
+}
+
+function draw() {
+  background(back_img);
+  
+   if (playerCount === 2) {
+     game.update(1);
+   }
+   if (gameState === 1) {
+     clear(); 
+     game.play();
+     if(keyDown("RIGHT_ARROW")){
+      M = M+10;
+  }
+  if(keyDown("LEFT_ARROW")){
+    M = M-10;
+}
+  console.log(M)
+   }
+   if (gameState === 2) {
+    
+     game.end();
+   }
+}
